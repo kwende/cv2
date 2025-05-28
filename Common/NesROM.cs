cec9a14bac665ee073f298721ce1836c4e389476
Ben Rush <brush@ocuvera.com>
2025-05-28 01:08:31 -0500

    Rearranging sprites for 8x16

---
+++ Common/NesROM.cs
@@ -0,0 +1,118 @@
+﻿using Common.Exceptions;
+using System.Runtime.InteropServices;
+
+namespace Common
+{
+    public class NesROM
+    {
+        private MemoryStream? _stream = null;
+        private iNesHeader _iNesHeader;
+        private const int SpriteSizeInBytes = 8;
+
+        private long _prgSize = 0, _chrSize = 0;
+
+        public NesROM()
+        {
+            _stream = new MemoryStream(275_000);
+        }
+
+        public static List<SpriteSheet> Transform8x16(List<SpriteSheet> input)
+        {
+            List<SpriteSheet> ret = new List<SpriteSheet>();
+            for (int i = 0; i < 16 * 16; i += 2)
+            {
+                var first = input[i];
+                var second = input[i + 1];
+            }
+
+            return ret;
+        }
+
+        public List<List<SpriteSheet>> GetSpriteSheets()
+        {
+            if (_stream == null || _prgSize == 0 || _chrSize == 0)
+            {
+                throw new NotInitializedException();
+            }
+            else
+            {
+                List<List<SpriteSheet>> sheets = new List<List<SpriteSheet>>();
+                _stream.Seek(_prgSize + 16, SeekOrigin.Begin);
+
+                byte[] chrBank = new byte[_chrSize];
+
+                _stream.Read(chrBank, 0, chrBank.Length);
+
+                // hack just for CV2 for now. 
+                int i = 0;
+                for (int tileNumber = 0; tileNumber < 32; tileNumber++)
+                {
+                    var sheet = new List<SpriteSheet>();
+                    sheets.Add(sheet);
+                    // each tile is a 16x16 grid of sprites
+                    for (int y = 0; y < 16; y++)
+                    {
+                        for (int x = 0; x < 16; x++)
+                        {
+                            var panel1 = new ReadOnlySpan<byte>(chrBank, i, SpriteSizeInBytes);
+                            i += 8;
+                            var panel2 = new ReadOnlySpan<byte>(chrBank, i, SpriteSizeInBytes);
+                            i += 8;
+
+                            int[] sheetData = new int[SpriteSizeInBytes * SpriteSizeInBytes];
+                            for (int _y = 0, k = 0; _y < SpriteSizeInBytes; _y++)
+                            {
+                                var panel1Byte = panel1[_y];
+                                var panel2Byte = panel2[_y];
+
+                                for (int _x = 0; _x < SpriteSizeInBytes; _x++, k++)
+                                {
+                                    byte mask = (byte)(0x80 >> _x);
+
+                                    int panel1Bit = (panel1Byte & mask) != 0 ? 1 : 0;
+                                    int panel2Bit = (panel2Byte & mask) != 0 ? 1 : 0;
+
+                                    int paletteIndex = (panel1Bit << 1) | panel2Bit;
+                                    sheetData[k] = paletteIndex;
+                                }
+                            }
+
+                            sheet.Add(new SpriteSheet(sheetData));
+                        }
+                    }
+                }
+                return sheets;
+            }
+        }
+
+        public async Task Load(Stream stream)
+        {
+            ArgumentNullException.ThrowIfNull(stream, nameof(stream));
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            await stream.CopyToAsync(_stream);
+
+            if (stream.Length < 16)
+            {
+                throw new InvalidRomSizeException();
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            byte[] headerBytes = new byte[16];
+            await stream.ReadAsync(headerBytes, 0, headerBytes.Length);
+
+            _iNesHeader = MemoryMarshal.Read<iNesHeader>(new ReadOnlySpan<byte>(headerBytes));
+
+            _prgSize = _iNesHeader.PrgROMSize * 0x4000L;
+            _chrSize = _iNesHeader.ChrROMSize * 0x2000L;
+
+            if (_iNesHeader.Magic != Constants.Magic ||
+                stream.Length < (16 + _prgSize + _chrSize))
+            {
+                throw new InvalidRomFormatException();
+            }
+        }
+    }
+}
---
+++ Common/SpriteSheet.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public class SpriteSheet
+    {
+        private int[] _sheetData;
+        public SpriteSheet(int[] sheetData)
+        {
+            _sheetData = sheetData;
+        }
+    }
+}
